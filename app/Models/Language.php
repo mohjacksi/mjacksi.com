84e5a78356821005facef70a229bcce1a3677cf9
Mohammed Jacksi <moh.jacksi@yahoo.com>
2022-09-24 11:10:34 +0300

    add soft delete to language

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Language extends Model
 {
 
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    
     protected $fillable = ['photo_id', 'id', 'name', 'is_default', 'code', 'rtl'];
 
     public function photo(){

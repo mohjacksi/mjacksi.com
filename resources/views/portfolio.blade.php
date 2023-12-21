b14f86e5e240d2489d2321a4d5cbedd911a7ae6d
Mohammed Jacksi <moh.jacksi@yahoo.com>
2023-12-21 15:24:32 +0300

    hide filter from protofolio

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -21,15 +21,15 @@
       <div class="container">
         <div class="row">
 
-            <div class="col-md-3">
+            {{-- <div class="col-md-3">
                 <div class="filters">
                     <h4>{{clean( trans('mjacksi-backend.sort_by') , array('Attr.EnableID' => true))}}</h4>
                     <div class="filter active" data-filter="all"><span>{{clean( trans('mjacksi-backend.all') , array('Attr.EnableID' => true))}}</span></div>
                     @foreach($project_categories as $category)
-                      <div class="filter inline clearBoth" data-filter="{{$category->name}}"><span>{{$category->name}}</span></div>
+                      <div class="filter" data-filter="{{$category->name}}"><span>{{$category->name}}</span></div>
                     @endforeach
                 </div>
-            </div>
+            </div> --}}
 
             <div class="col-md-12">
                   <div class="projects projects-page row">
